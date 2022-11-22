c1048ee1bb3d0d0f19a9784931ccc73646f54be8
Samuel_Lehnen <samuel.lehnen@katek-group.com>
2022-11-22 16:18:34 +0100

    RestAPI Interface gebaut

--- src/Connectivity/RestApi.h
+++ src/Connectivity/RestApi.h
@@ -9,13 +9,17 @@
 namespace Connectivity
 {
     using JsonCallback = std::function<void(json)>;
+    using JsonGenerator = std::function<json()>;
 
     class RestAPI
     {
     public:
         RestAPI(AsyncWebServer server, std::string baseUri = "/", bool allowCors = true);
-        createEndpointRead(std::string endpointUri, std::string filePath, JsonCallback callback = [](json){});
-        createEndpointWrite(std::string endpointUri, std::string filePath);
+        createEndpointRead(std::string endpointUri, std::string filePath);
+        createEndpointRead(std::string endpointUri, JsonGenerator generator);
+        createEndpointWrite(std::string endpointUri, std::string filePath, JsonCallback callback = [](json){});
+        createEndpointWrite(std::string endpointUri, JsonCallback callback = [](json){});
+
         
 
     private:
